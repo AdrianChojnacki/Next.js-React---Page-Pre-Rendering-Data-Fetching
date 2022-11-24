a18011f0a3f311e16887a5fe0fe8c97f1e01ab54
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2022-11-24 10:59:42 +0100

    Add not-working version of first stage

---
+++ helpers/api-util.js
@@ -0,0 +1,23 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://nextjs-2-801df-default-rtdb.firebaseio.com/Events.json"
+  );
+  const data = await response.json();
+  console.log(data);
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
